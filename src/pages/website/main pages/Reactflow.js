db569ac6c8c5c3fc50621c9c6bd48f9b68e768f6
amine <dzsmasharabic@gmail.com>
2025-02-08 14:25:13 +0100

    first commit

--- src/pages/website/main pages/Reactflow.js
+++ src/pages/website/main pages/Reactflow.js
@@ -508,8 +508,8 @@ setOpen3(true);
 
 
 
-{saveAlerts&& <Alert style={{position:"absolute",top:"20%",right:'50%'}} severity="success">Save Success</Alert> }
-{saveAlert&& <Alert style={{position:"absolute",top:"20%",right:'50%'}}  severity="error">Save Failed</Alert> }
+{saveAlerts&& <Alert style={{position:"absolute",top:"100px",right:'50%'}} severity="success">Save Success</Alert> }
+{saveAlert&& <Alert style={{position:"absolute",top:"100px",right:'50%'}}  severity="error">Save Failed</Alert> }
 
       <ReactFlow
         nodes={nodes}
